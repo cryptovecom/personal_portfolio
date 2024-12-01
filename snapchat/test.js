e5bc1ea92bf744cf7f11b1c9195c5253e7516511
cryptovecom <rwdev012995@gmail.com>
2024-12-01 15:30:19 +0100

    test.js file delete

--- snapchat/test.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.toggle-button').forEach(button => {
-    button.addEventListener('click', () => {
-      const items = button.nextElementSibling;
-      if (items) {
-        items.style.display = items.style.display === 'block' ? 'none' : 'block';
-      }
-    });
-  });
